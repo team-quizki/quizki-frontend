b8bf6a1cee74978d4217f662c506029356b71093
FrauDeborah <FrauDeborah@gmail.com>
2018-10-14 20:12:33 -0600

    http put request still not working. Updated @angular cli to 6.2.5 and associated updates

--- src/app/registration/register.service.ts
+++ src/app/registration/register.service.ts
@@ -1,44 +1,58 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
-import { HandleError } from './../http-error-handler.service';
-import { UserNameEmail } from './../user/userNameEmail';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
-};
+const isUniqueURL = 'http://localhost:8080/api/users/isUnique';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  isUniqueURL = 'http://localhost:8080/api/users/isUnique';
-  handleError : undefined; /* can't seam to get the handleError to work  */
-
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient) { }
 
   isUniqueName( name: string ) : Observable<string> {
     console.log('in isUniqueName ' + name);
-    let nameString = '{"name":"' + name + '"}';
-
+    let nameInJSONString = '{"name":"' + name + '"}';
+    console.log(nameInJSONString);
+    
+    let httpOptions = {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')//,
+    //  responseType: 'json'
+    };
+    return this.http.post<string>(isUniqueURL,
+      nameInJSONString, httpOptions);
+/*
     return this.http.post<string>(this.isUniqueURL,
-      nameString,
+      nameInJSONString,
       {
         headers: new HttpHeaders()
-            .set('content-type',"application/json"),
+            .set('content-type','application/json'),
         responseType: 'json'
       }
     );
-    /*
-    return this.http.post<string>(this.isUniqueURL, name, httpOptions)
+*/
+
+    /* This is the description for a post returning json
+
+
+      post(url: string, body: any | null, options?: {
+        headers?: HttpHeaders | {
+            [header: string]: string | string[];
+        };
+        observe?: 'body';
+        params?: HttpParams | {
+            [param: string]: string | string[];
+        };
+        reportProgress?: boolean;
+        responseType?: 'json';
+        withCredentials?: boolean;
+      }): Observable<Object>
+
+    return this.http.post<string>(this.isUniqueURL, nameAsJSON, httpOptions)
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError('isUnique', name))
       );
     */
   }
