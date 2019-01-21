d43fe6ccc0d9c60c2b69cf475f3cd91d9a0ff36e
FrauDeborah <FrauDeborah@gmail.com>
2019-01-21 02:41:32 -0700

    devApiTests removed all tests and added test for apiService.get() which is failing. pushing to let mentor look at the test. ng serve builds. ng lint clean.

--- src/app/_services/api.service.spec.ts
+++ src/app/_services/api.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
 
 import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
@@ -8,180 +8,63 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { ApiService } from './api.service';
 import { environment } from './../../environments/environment';
 
+import { Role, User } from './../user/user';
+
+const baseUrl = environment.apiUrl;
+const testUsername = 'mouse';
+const testPassword = 'password';
+
 describe('ApiService', () => {
-
-  let service: ApiService;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
-
-  const testFakeUrl = '/api/testFakeUrl';
-  const testBaseFakeUrl = environment.apiUrl + testFakeUrl;
-  const testUsername = 'username';
-  const testPassword = 'password';
-  const testFakeData = {name: 'Test Fake Data'};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
       providers: [ ApiService ]
     });
-
-    // Inject the http service and test controller for each test
-    httpClient = TestBed.get(HttpClient);
-    httpTestingController = TestBed.get(HttpTestingController);
-    service = TestBed.get(ApiService);
   });
 
-  afterEach(() => {
-    // After every test, assert that there are no more pending requests.
-    httpTestingController.verify();
-  });
-
-  it('#api.getHttpHeaders should be defined ', () => {
-    expect(service.getHttpHeaders()).toBeDefined();
-  });
-
-  it('#api.getHttpHeadersWithBase64Authorization should be defined ', () => {
-    expect(service.getHttpHeadersWithBase64Authorization(testUsername, testPassword)).toBeDefined();
-  });
-
-  it('#api.get should be defined ', () => {
-    expect(service.get(testFakeUrl, testUsername, testPassword)).toBeDefined();
-  });
-
-  it('#api.get can test HttpClient.get', () => {
-  //  const testFakeData = {name: 'Test Fake Data'};
-    const testHeader = service.getHttpHeadersWithBase64Authorization(testUsername, testPassword);
-    // Make an HTTP GET request
-    httpClient.get<any>(testBaseFakeUrl, testHeader)
-      .subscribe(data =>
-        // When observable resolves, result should match test data
-        expect(data).toEqual(testFakeData)
-      );
-
-    // The following `expectOne()` will match the request's URL.
-    // If no requests or multiple requests matched that URL
-    // `expectOne()` would throw.
-    const req = httpTestingController.expectOne(testBaseFakeUrl);
-
-    // Assert that the request is a GET.
-    expect(req.request.method).toEqual('GET');
-
-    // Respond with mock data, causing Observable to resolve.
-    // Subscribe callback asserts that correct data was returned.
-    req.flush(testFakeData);
-  });
-
-  it('#api.get can test for 404 error', () => {
-    const errorMessage = 'deliberate 404 error';
-    const testHeader = service.getHttpHeadersWithBase64Authorization(testUsername, testPassword);
-
-    httpClient.get<any>(testBaseFakeUrl, testHeader).subscribe(
-      data => fail('should have failed with the 404 error'),
-      (error: HttpErrorResponse) => {
-        expect(error.status).toEqual(404, 'status');
-        expect(error.error).toEqual(errorMessage, 'message');
-      }
-    );
-
-    const req = httpTestingController.expectOne(testBaseFakeUrl);
-
-    // Respond with mock error
-    req.flush(errorMessage, { status: 404, statusText: 'Not Found' });
-  });
-
-  it('#api.get can test for network error', () => {
-    const errorMessage = 'simulated network error';
-    const testHeader = service.getHttpHeadersWithBase64Authorization(testUsername, testPassword);
-
-    httpClient.get<any>(testBaseFakeUrl, testHeader).subscribe(
-      data => fail('should have failed with the network error'),
-      (error: HttpErrorResponse) => {
-        expect(error.error.message).toEqual(errorMessage, 'message');
-      }
-    );
-
-    const req = httpTestingController.expectOne(testBaseFakeUrl);
-
-    // Create mock ErrorEvent, raised when something goes wrong at the network level.
-    // Connection timeout, DNS error, offline, etc
-    const mockError = new ErrorEvent('Network error', {
-      message: errorMessage,
-    });
-
-    // Respond with mock error
-    req.error(mockError);
-  });
-
-  it('#api.post should be defined ', () => {
-    const testPostHeader = service.getHttpHeaders();
-    expect(httpClient.post(testFakeUrl, testFakeData, testPostHeader)).toBeDefined();
-  });
-
-  it('#api.post can test HttpClient.post', () => {
-    const testPostHeader = service.getHttpHeaders();
-
-    // Make an HTTP POST request
-    httpClient.post<any>(testBaseFakeUrl, testFakeData, testPostHeader)
-      .subscribe((res) => {
-        expect(res).toEqual(testFakeData);
-      });
-
-    // The following `expectOne()` will match the request's URL.
-    // If no requests or multiple requests matched that URL
-    // `expectOne()` would throw.
-    const req = httpTestingController.expectOne(testBaseFakeUrl);
-
-    // Assert that the request is a GET.
-    expect(req.request.method).toEqual('POST');
-
-    // Respond with mock data, causing Observable to resolve.
-    // Subscribe callback asserts that correct data was returned.
-    req.flush(testFakeData);
-  });
-
-  it('#api.post can test for 404 error', () => {
-    const errorMessage = 'deliberate 404 error';
-    const testPostHeader = service.getHttpHeaders();
-
-    httpClient.post<any>(testBaseFakeUrl, testFakeData, testPostHeader).subscribe(
-      data => fail('should have failed with the 404 error'),
-      (error: HttpErrorResponse) => {
-        expect(error.status).toEqual(404, 'status');
-        expect(error.error).toEqual(errorMessage, 'message');
-      }
-    );
-
-    const req = httpTestingController.expectOne(testBaseFakeUrl);
-
-    // Respond with mock error
-    req.flush(errorMessage, { status: 404, statusText: 'Not Found' });
-  });
-
-  it('#api.post can test for network error', () => {
-    const errorMessage = 'simulated network error';
-    const testPostHeader = service.getHttpHeaders();
-
-    httpClient.post<any>(testBaseFakeUrl, testFakeData, testPostHeader).subscribe(
-      data => fail('should have failed with the network error'),
-      (error: HttpErrorResponse) => {
-        expect(error.error.message).toEqual(errorMessage, 'message');
-      }
-    );
-
-    const req = httpTestingController.expectOne(testBaseFakeUrl);
-
-    // Create mock ErrorEvent, raised when something goes wrong at the network level.
-    // Connection timeout, DNS error, offline, etc
-    const mockError = new ErrorEvent('Network error', {
-      message: errorMessage,
-    });
-
-    // Respond with mock error
-    req.error(mockError);
-  });
-
-  it('#api.service should be created', () => {
+  it('should be created',
+    inject([ApiService], (service: ApiService) => {
     expect(service).toBeTruthy();
-  });
+  }));
+
+  it('tests apiService.get()',
+    // Verify that a call to apiService.get() will load the apiUrl
+    // we give it, and will return the correct data to whatever
+    // code called the apiService.get()
+
+    inject([HttpTestingController, ApiService],
+      (httpMock: HttpTestingController, service: ApiService) => {
+        const url = '/api/verifyCredentials';
+
+        service.get(url, testUsername, testPassword).subscribe((data: User) => {
+          expect(data).toBeDefined();
+          expect(data.id).toEqual(10);
+          expect(data.role.id).toEqual(2);
+          expect(data.role.name).toEqual('QUIZKI_USER_ROLE_USER');
+          expect(data.password).toBeNull();
+          expect(data.name).toEqual('mouse');
+          expect(data.enabled).toEqual(1);
+          expect(data.email).toEqual('mouse@fakeMice.com');
+          expect(data.fullname).toEqual('Toy Mouse');
+          expect(data.demographic).toEqual('independent');
+        });
+
+        const req = httpMock.expectOne(baseUrl + url);
+        expect(req.request.method).toEqual('GET');
+
+        req.flush( {data: {'id': 10, 'role': {'id': 2, 'name': 'QUIZKI_USER_ROLE_USER'},
+          'password': '', 'name': 'mouse', 'enabled': 1, 'email': 'mouse@fakeMice.com',
+          'fullname': 'Toy Mouse', 'demographic': 'independent'
+         }});
+      })
+  );
+
+  afterEach(
+    inject([HttpTestingController],
+      (httpMock: HttpTestingController) => {
+        httpMock.verify();
+      })
+  );
+
 });
