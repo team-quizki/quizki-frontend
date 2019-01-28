4df2f67becb7b26cfc6bba2ad67b65a3976016a4
FrauDeborah <FrauDeborah@gmail.com>
2019-01-28 15:33:40 -0700

    devRegTests modified to have spys on apiService. Not sure what afterEach should clean up?

--- src/app/registration/register.service.spec.ts
+++ src/app/registration/register.service.spec.ts
@@ -2,18 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { RegisterService } from './register.service';
 import { ApiService } from '../_services/api.service';
-import { UserRegistration } from '../user/user-registration';
 import { User, Role } from '../user/user';
-
-const uniqueUsername = 'uniqueUsername';
-const duplicateUsername = 'duplicateUsername';
-const uniqueEmail = 'uniqueEmail@junk.com';
-const duplicateEmail = 'duplicateEmail@junk.com';
-const userRegistration = new UserRegistration('abc123', uniqueUsername, 2, uniqueEmail, 'Test Success Fullname');
-const role = new Role(2, 'userRole 2 definition');
-const user = new User(20, role, 'abc123', uniqueUsername, 1, uniqueEmail, 'Test Fullname', true, 'demograhic indicator' );
-const userBadRegistration = new UserRegistration('abc123', duplicateUsername, 2, duplicateEmail, 'Test Failed Fullname');
-const failedUserReg = '{"errors": [{"field": "email","message": "email.not.unique"}, {"field": "name", "message": "name.not.unique"}]}';
 
 describe('RegisterService', () => {
   beforeEach(() => {
@@ -29,34 +18,80 @@ describe('RegisterService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('isUniqueUsername(uniqueUsername) should be truthy', inject([RegisterService], (service: RegisterService) => {
-    spyOn(service, 'isUniqueUsername').and.returnValue(Promise.resolve(true));
-    expect(service.isUniqueUsername(uniqueUsername)).toBeTruthy();
+  it('isUniqueUsername should have truthy apiService.post with returnValue true', inject([RegisterService, ApiService],
+    (registerService: RegisterService, apiService: ApiService) => {
+
+    apiService.post = jasmine.createSpy().and.returnValue(true);
+    expect(apiService.post).toBeTruthy();
+
+    registerService.isUniqueUsername = jasmine.createSpy().and.returnValue(apiService.post);
+    expect(registerService.isUniqueUsername).toBeTruthy();
+
   }));
 
-  it('isUniqueUsername(duplicateUsername) should be truthy', inject([RegisterService], (service: RegisterService) => {
-    spyOn(service, 'isUniqueUsername').and.returnValue(Promise.resolve(false));
-    expect(service.isUniqueUsername(duplicateUsername)).toBeTruthy();
+  it('isUniqueUsername should have truthy apiService.post with returnValue false', inject([RegisterService, ApiService],
+    (registerService: RegisterService, apiService: ApiService) => {
+
+    apiService.post = jasmine.createSpy().and.returnValue(false);
+    expect(apiService.post).toBeTruthy();
+
+    registerService.isUniqueUsername = jasmine.createSpy().and.returnValue(apiService.post);
+    expect(registerService.isUniqueUsername).toBeTruthy();
+
   }));
 
-  it('isUniqueEmail(uniqueEmail) should be truthy', inject([RegisterService], (service: RegisterService) => {
-    spyOn(service, 'isUniqueEmail').and.returnValue(Promise.resolve(true));
-    expect(service.isUniqueEmail(uniqueEmail)).toBeTruthy();
+  it('isUniqueEmail should have truthy apiService.post with returnValue true', inject([RegisterService, ApiService],
+    (registerService: RegisterService, apiService: ApiService) => {
+
+    apiService.post = jasmine.createSpy().and.returnValue(true);
+    expect(apiService.post).toBeTruthy();
+
+    registerService.isUniqueEmail = jasmine.createSpy().and.returnValue(apiService.post);
+    expect(registerService.isUniqueEmail).toBeTruthy();
+
   }));
 
-  it('isUniqueEmail(duplicateEmail) should be truthy', inject([RegisterService], (service: RegisterService) => {
-    spyOn(service, 'isUniqueEmail').and.returnValue(Promise.resolve(false));
-    expect(service.isUniqueEmail(duplicateEmail)).toBeTruthy();
+  it('isUniqueEmail should have truthy apiService.post with returnValue false', inject([RegisterService, ApiService],
+    (registerService: RegisterService, apiService: ApiService) => {
+
+    apiService.post = jasmine.createSpy().and.returnValue(false);
+    expect(apiService.post).toBeTruthy();
+
+    registerService.isUniqueEmail = jasmine.createSpy().and.returnValue(apiService.post);
+    expect(registerService.isUniqueEmail).toBeTruthy();
+
   }));
 
-  it('registerUser(userRegistration) should be truthy', inject([RegisterService], (service: RegisterService) => {
-    spyOn(service, 'registerUser').and.returnValue(Promise.resolve(user));
-    expect(service.registerUser(userRegistration)).toBeTruthy();
+  it('registerUser should have truthy apiService.post with returnValue user', inject([RegisterService, ApiService],
+    (registerService: RegisterService, apiService: ApiService) => {
+
+    const role = new Role(2, 'userRole 2 definition');
+    const user = new User(20, role, 'abc123', 'uniqueUsername', 1, 'uniqueEmail', 'Test Fullname', true, 'demograhic indicator' );
+
+    apiService.post = jasmine.createSpy().and.returnValue(user);
+    expect(apiService.post).toBeTruthy();
+
+    registerService.registerUser = jasmine.createSpy().and.returnValue(apiService.post);
+    expect(registerService.registerUser).toBeTruthy();
+
   }));
 
-  it('registerUser(userBadRegistration) should be truthy', inject([RegisterService], (service: RegisterService) => {
-    spyOn(service, 'registerUser').and.returnValue(Promise.resolve(failedUserReg));
-    expect(service.registerUser(userBadRegistration)).toBeTruthy();
+  it('registerUser should have truthy apiService.post with returnValue failedUserReg', inject([RegisterService, ApiService],
+    (registerService: RegisterService, apiService: ApiService) => {
+
+    const failedUserReg = '{"errors": [{"field": "email","message": "email.not.unique"}, {"field": "name", "message": "name.not.unique"}]}';
+
+    apiService.post = jasmine.createSpy().and.returnValue(failedUserReg);
+    expect(apiService.post).toBeTruthy();
+
+    registerService.registerUser = jasmine.createSpy().and.returnValue(apiService.post);
+    expect(registerService.registerUser).toBeTruthy();
+
   }));
+
+  afterEach(() => {
+    // nothing to clean up???
+  });
+
 
 });
