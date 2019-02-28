d5064405148029dd8ffc76b7321f8f9e9f3274fc
FrauDeborah <FrauDeborah@gmail.com>
2019-02-27 19:36:01 -0700

    added modal tests to loginDialogService

--- src/app/login/login-dialog.service.spec.ts
+++ src/app/login/login-dialog.service.spec.ts
@@ -1,30 +1,65 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule, MatDialogRef, MatDialog } from '@angular/material';
+import { TestBed, inject } from '@angular/core/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule, MatDialog, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
 
 import { LoginDialogService } from './login-dialog.service';
 import { LoginComponent } from './login.component';
+import { NgModule } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import {  ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../_services/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+const TEST_DIRECTIVES = [
+  LoginComponent
+];
+
+@NgModule({
+  imports: [MatDialogModule, NoopAnimationsModule, MatCardModule, ReactiveFormsModule, BrowserAnimationsModule,
+    MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, HttpClientModule, HttpClientTestingModule ],
+  exports: TEST_DIRECTIVES,
+  declarations: TEST_DIRECTIVES,
+  entryComponents: [
+    LoginComponent
+  ],
+})
+class DialogTestModule { }
 
 describe('LoginDialogService', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
 
-  let matDialogRefStub;
-  let matDialogStub;
+  let matDialog: MatDialog
+  let overlayContainerElement: HTMLElement;
 
   beforeEach(() => {
-    matDialogRefStub = jasmine.createSpyObj('loginMatDialogRefStub', ['matDialogRef'] );
-    matDialogStub = jasmine.createSpyObj('loginMatDialogStub', ['matDialog'] );
 
     TestBed.configureTestingModule({
       providers: [
         { provide: LoginDialogService, useClass: LoginDialogService },
-        { provide: MatDialogRef, useValue: matDialogRefStub },
-        { provide: MatDialog, useValue: matDialogStub }
+        { provide: OverlayContainer, useFactory: () => {
+          overlayContainerElement = document.createElement('div');
+          return { getContainerElement: () => overlayContainerElement };
+        }},
+        {provide: ApiService, useClass: ApiService}
       ],
-      imports: [ BrowserAnimationsModule ]
+      imports: [ DialogTestModule ]
     });
+
+     matDialog = TestBed.get(MatDialog);
+  });
+
+  it('shows login button is present', () => {
+    matDialog.open(LoginComponent);
+    const loginButton = overlayContainerElement.querySelector('#login_button');
+
+    expect(loginButton.textContent).toBe('Login ')
+  });
+
+  it('shows cancel button is present', () => {
+    matDialog.open(LoginComponent);
+    const cancelButton = overlayContainerElement.querySelector('#cancel_button');
+
+    expect(cancelButton.textContent).toBe('Cancel ')
   });
 
   it('should be created', inject([LoginDialogService], (service: LoginDialogService) => {
