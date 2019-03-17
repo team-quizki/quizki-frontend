be99c2d1181dcb69378d2c2498993ae1e1c5e3cd
FrauDeborah <FrauDeborah@gmail.com>
2019-03-17 17:54:15 -0600

    removed all code related to legacy questions and questionsData

--- src/app/create-question-page/create-question-page.component.spec.ts
+++ src/app/create-question-page/create-question-page.component.spec.ts
@@ -9,18 +9,6 @@ import { CreateQuestionPageTopicSectionComponent } from './create-question-page-
 import { CreateQuestionPageEditorSectionComponent } from './create-question-page-editor-section/create-question-page-editor-section.component';
 import { ApiService } from '../_services/api.service';
 
-import { QuestionsComponent }           from '../questions/questions.component'
-import { QuestionComponent }            from '../questions/question/question.component'
-import { QuestionTopicsComponent }      from '../questions/question/question-topics/question-topics.component'
-import { QuestionTopicComponent }       from '../questions/question/question-topics/question-topic/question-topic.component'
-import { QuestionReferencesComponent }  from '../questions/question/question-references/question-references.component'
-import { QuestionReferenceComponent }   from '../questions/question/question-references/question-reference/question-reference.component'
-import { QuestionChoicesComponent }     from '../questions/question/question-choices/question-choices.component'
-import { QuestionChoiceComponent }      from '../questions/question/question-choices/question-choice/question-choice.component'
-import { QuestionDifficultyComponent }  from '../questions/question/question-difficulties/question-difficulty.component'
-import { QuestionTypeComponent }        from '../questions/question/question-types/question-type.component'
-
-import { QuestionDataService }          from '../questionData/question-data.service'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // tinymce is causing "Uncaught TypeError:Cannot read property setAttribute of undefined"
@@ -34,15 +22,11 @@ xdescribe('CreateQuestionPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateQuestionPageComponent, CreateQuestionPageTopicSectionComponent,
-        CreateQuestionPageEditorSectionComponent,
-        QuestionsComponent, QuestionComponent, QuestionTopicsComponent, QuestionTopicComponent, QuestionReferencesComponent, QuestionReferenceComponent, QuestionTypeComponent, QuestionChoicesComponent, QuestionChoiceComponent, QuestionDifficultyComponent ],
+        CreateQuestionPageEditorSectionComponent ],
       imports: [ BrowserAnimationsModule, FormsModule, EditorModule, FormsModule, MatFormFieldModule, MatCheckboxModule,
         MatCardModule, MatOptionModule, MatSelectModule, MatSnackBarModule, MatInputModule ],
       providers: [
-        { provide: ApiService, useValue: { postCreatedQuestion: jasmine.createSpy('postCreatedQuestion') }},
-        {
-          provide: QuestionDataService, useValue: { getQuestions: jasmine.createSpy('getQuestions'), getQuestionTypeData: jasmine.createSpy('getQuestionTypeData')}
-        }
+        { provide: ApiService, useValue: { postCreatedQuestion: jasmine.createSpy('postCreatedQuestion') }}
       ]
     })
     .compileComponents();
