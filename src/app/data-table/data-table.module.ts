68c6297cd7b433d6fb5361d85f6aef4d61cc1325
Johnathan James <haxwell@gmail.com>
2020-05-05 06:36:16 -0600

    Added a missing module import

--- src/app/data-table/data-table.module.ts
+++ src/app/data-table/data-table.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import {MatButtonModule} from '@angular/material/button';
 
